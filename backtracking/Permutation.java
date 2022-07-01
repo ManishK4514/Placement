86f1a23bc627d0897a01d0712b4e3f562ee551c7
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-07-01 16:32:46 +0530

    Add files via upload
    First commit

---
+++ backtracking/Permutation.java
@@ -0,0 +1,19 @@
+package backtracking;
+
+public class Permutation {
+    public static void PrintPermutation(String str, String combination){
+        if(str.length() == 0){
+            System.out.println(combination);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+            String newString = str.substring(0, i) + str.substring(i + 1);
+            PrintPermutation(newString, combination + currChar);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "ABC";
+        PrintPermutation(str, "");
+    }
+}
